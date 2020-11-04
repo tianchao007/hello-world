21338825985a07e4cdafdcd7d32ae2b6abf96107
tianchao <tianchao001@sinosoft.com.cn>
2020-11-04 17:13:59 +0800

    test

---
+++ src/main/java/com/sinosoft/test/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.sinosoft.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping(value = "/hello",method = {RequestMethod.GET})
+    public String hello(){
+        return "hello";
+    }
+}
